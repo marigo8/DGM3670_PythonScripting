e965104425b20a2fb1cdc381c17188acd5be0598
Mario Tijerino <mariogotijerino@gmail.com>
2021-12-02 14:11:18 -0700

    Classes

--- windows.py
+++ windows.py
@@ -1,15 +1,39 @@
 import maya.cmds as cmds
 
-window = cmds.window(title="My Window", width=500, height=500)
-column = cmds.columnLayout(parent=window)
-cmds.button(parent=column, label="Create Empty Window", command="create_empty_window()")
-cmds.floatSlider(parent=column)
-cmds.floatSliderGrp(parent=column, label='Group 1', field=True)
-cmds.floatSliderGrp(parent=column, label='Group 2', field=True, minValue=-10.0, maxValue=10.0, fieldMinValue=-100.0,
-                    fieldMaxValue=100.0, value=0)
-cmds.colorIndexSliderGrp(parent=column, label="Color")
+class ToolUI():
 
-cmds.showWindow(window)
+    def __init__(self):
+        self.m_window = 'changeColorUIWin'
+
+    def create(self):
+        self.delete()
+
+        self.m_window = cmds.window(self.m_window, title="My Window", width=500, height=500)
+        column = cmds.columnLayout(parent=self.m_window)
+        cmds.button(parent=column, label="Create Empty Window", command=lambda x: create_empty_window())
+        cmds.floatSlider(parent=column)
+        cmds.floatSliderGrp(parent=column, label='Group 1', field=True)
+        cmds.floatSliderGrp(parent=column, label='Group 2', field=True, minValue=-10.0, maxValue=10.0,
+                            fieldMinValue=-100.0,
+                            fieldMaxValue=100.0, value=0)
+        cmds.colorIndexSliderGrp(parent=column, label="Color", maxValue=32)
+
+        self.show()
+
+    def delete(self):
+        if cmds.window(self.m_window, exists=True):
+            cmds.deleteUI(self.m_window)
+
+    def show(self):
+        if cmds.window(self.m_window, exists=True):
+            cmds.showWindow(self.m_window)
+
+myWindow = ToolUI()
+myWindow.create()
+help(myWindow)
+
+myWindow.show()
+
 
 
 def create_empty_window():
