84321dbd0e447420f56eb28dc91033f68de99d4f
Mario Tijerino <mariogotijerino@gmail.com>
2021-12-15 22:21:02 -0700

    Finalized Tool UI

--- tools.py
+++ tools.py
@@ -2,16 +2,14 @@ import maya.cmds as cmds
 
 
 def change_color(name, color_index):
-    nodeType = cmds.nodeType(name)
     shape = ''
-    if nodeType is "mesh" or nodeType is "nurbsCurve":
+    if cmds.attributeQuery('overrideColor', exists=True, node=name):
         shape = name
     else:
         shapes = cmds.listRelatives(name, shapes=True)
         if shapes is None:
             return
         shape = shapes[0]
-        nodeType = cmds.nodeType(shape)
 
     cmds.setAttr(f"{shape}.overrideEnabled", 1)
     cmds.setAttr(f"{shape}.overrideColor", color_index)
@@ -74,3 +72,38 @@ def create_single_control(name="Control", color_index=0):
 
     group = cmds.group(control, name=group_name)
     return [group, control]
+
+
+def sequential_rename_selection(name_pattern, start_num=1):
+
+    # Get Selection #
+    sels = cmds.ls(sl=True)
+
+    # number of zeroes needed for zfill #
+    z_count = name_pattern.count("#")
+
+    # error out if no "#" symbols were found #
+    if z_count == 0:
+        print("name_pattern must include \"#\" symbols")
+        return
+
+    # generate "#" symbols for partition #
+    z_place_holder = ""
+    for z in range(z_count):
+        z_place_holder += "#"
+
+    # partition #
+    par = name_pattern.partition(z_place_holder)
+
+    # error out if "#" symbols are in multiple parts of the string
+    if par[1] != z_place_holder:
+        print("\"#\" symbols cannot be in multiple parts of the name_pattern")
+        return
+
+    # rename #
+    for i in range(len(sels)):
+        new_name = par[0]
+        new_name += str(start_num + i).zfill(z_count)
+        new_name += par[2]
+        obj = cmds.rename(sels[i], new_name)
+        print(f"{sels[i]} renamed to {obj}")
--- tools_ui.py
+++ tools_ui.py
@@ -7,25 +7,32 @@ class tools_ui:
         self.m_window = 'toolsUIWin'
         self.override_color = 0
         self.colorSlider = ''
+        self.renameField = ''
 
     def create(self):
         self.delete()
 
-        self.m_window = cmds.window(self.m_window, title="Tools")
+        self.m_window = cmds.window(self.m_window, title="Tools", resizeToFitChildren=True)
         column = cmds.columnLayout(parent=self.m_window)
 
         # sequential renamer
+        self.renameField = cmds.textFieldButtonGrp(parent=column,
+                                label="Sequential Renamer",
+                                text="arm_##_jnt",
+                                buttonLabel="Rename",
+                                buttonCommand=lambda *x: self.sequential_rename_cmd())
 
         # change color
         self.colorSlider = cmds.colorIndexSliderGrp(parent=column,
                                                     label="OverrideColor",
                                                     minValue=1,
-                                                    maxValue=32,
-                                                    changeCommand=lambda *x: self.override_color_select_cmd())
-        cmds.button(parent=column, label="Apply Color To Selection", command=lambda *x: self.override_color_apply_cmd())
+                                                    maxValue=32)
+        cmds.button(parent=column, label="Apply Color", command=lambda *x: self.override_color_apply_cmd())
+        cmds.button(parent=column, label="Apply Color To Hierarchy", command=lambda *x: self.override_color_apply_hierarchy_cmd())
 
         # controls
         cmds.button(parent=column, label="Create Controls", command=lambda *x: self.create_controls_cmd())
+        cmds.button(parent=column, label="Create Controls From Hierarchy", command=lambda *x: self.create_controls_hierarchy_cmd())
 
         self.show()
 
@@ -37,19 +44,51 @@ class tools_ui:
         if cmds.window(self.m_window, exists=True):
             cmds.showWindow(self.m_window)
 
-    def override_color_select_cmd(self):
-        self.override_color = cmds.colorIndexSliderGrp(self.colorSlider, q=True, value=True) - 1
+    def get_color(self):
+        return cmds.colorIndexSliderGrp(self.colorSlider, q=True, value=True) - 1
 
     def override_color_apply_cmd(self):
         import tools
         importlib.reload(tools)
         sels = cmds.ls(sl=True)
         for sel in sels:
-            tools.change_color(sel, self.override_color)
+            tools.change_color(sel, self.get_color())
         return
+
+
+    def override_color_apply_hierarchy_cmd(self):
+        import tools
+        importlib.reload(tools)
+        sels = cmds.ls(sl=True)
+        for sel in sels:
+            tools.change_color(sel, self.get_color())
+            children = cmds.listRelatives(sel, allDescendents=True)
+            for child in children:
+                tools.change_color(child, self.get_color())
+        return
+
 
     def create_controls_cmd(self):
         import tools
         importlib.reload(tools)
 
-        tools.create_controls_hierarchy(self.override_color)
+        tools.create_controls(self.get_color())
+
+
+    def create_controls_hierarchy_cmd(self):
+        import tools
+        importlib.reload(tools)
+
+        tools.create_controls_hierarchy(self.get_color())
+
+    def sequential_rename_cmd(self):
+        import tools
+        importlib.reload(tools)
+
+        name_pattern = cmds.textFieldButtonGrp(self.renameField, q=True, text=True)
+        tools.sequential_rename_selection(name_pattern)
+
+
+# Demonstration
+my_ui = tools_ui()
+my_ui.create()
